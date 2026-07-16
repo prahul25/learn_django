8e043e581643820fca8c99b00c86c275b3b8cd39
prahul25 <palrahulkumar2002@gmail.com>
2026-07-16 07:57:07 +0530

    feat: complete chai catalog with Django relationships, Tailwind v4, admin customization, and media handling

--- learn_django/first_app/admin.py
+++ learn_django/first_app/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Chaia_Variety
-# Register your models here.
+from .models import Chaia_Variety,Store,Chai_Review,ChaiCertificate
 
-admin.site.register(Chaia_Variety)
+
+# Register your models here.
+class ChaiReviewInline(admin.TabularInline):
+    model = Chai_Review
+    extra = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ("name",'type','date_added','price','description')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ("name","location")
+    filter_horizontal = ("chai_varities",)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ("chai", "certificate_number")
+    
+admin.site.register(Chaia_Variety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
--- learn_django/first_app/models.py
+++ learn_django/first_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Chaia_Variety(models.Model):
@@ -15,6 +16,40 @@ class Chaia_Variety(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
     description = models.TextField(default='')
+    price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
 
     def __str__(self):
         return self.name
+    
+
+#ONE TO MANY
+class Chai_Review(models.Model):
+    chai = models.ForeignKey(Chaia_Variety, on_delete=models.CASCADE,related_name="reviews")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField("")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review for {self.chai.name}"
+    
+# MANY TO MANY RELATION
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varities = models.ManyToManyField(Chaia_Variety,related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+# ONE TO ONE
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(Chaia_Variety,on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_data = models.DateTimeField(default=timezone.now)
+    valid_untill = models.DateField()
+
+    def __str__(self):
+        return f"Certificate for {self.chai.name}"
